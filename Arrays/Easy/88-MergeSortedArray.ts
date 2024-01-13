6a7b503286eb89e096190ab91dd603ec5abd18e5
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2024-01-13 12:55:09 +0200

    Solved problem number 88 Merge Sorted Array

---
+++ Arrays/Easy/88-MergeSortedArray.ts
@@ -0,0 +1,19 @@
+/**
+    Easy
+    https://leetcode.com/problems/merge-sorted-array/
+*/
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+};
