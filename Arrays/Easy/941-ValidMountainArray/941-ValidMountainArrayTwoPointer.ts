7cdd9cdbc83f31e5e61d7ce8850d5e3335487e85
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2024-01-16 14:30:31 +0200

    Solved problem number 941 using two pointers

---
+++ Arrays/Easy/941-ValidMountainArray/941-ValidMountainArrayTwoPointer.ts
@@ -0,0 +1,19 @@
+/**
+    Easy
+    https://leetcode.com/problems/valid-mountain-array/
+*/
+function validMountainArray(arr: number[]): boolean {
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left + 2 < arr.length && arr[left] < arr[left + 1]) {
+        left++;
+    }
+
+    while (right > 1 && arr[right] < arr[right - 1]) {
+        right--;
+    }
+
+    return left === right && arr.length > 1;
+};
