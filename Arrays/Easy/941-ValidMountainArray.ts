8d9401dedc98860a70897f374bfafa6590d82e65
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2024-01-16 14:05:29 +0200

    Solved problem number 941 Valid Mountain Array

---
+++ Arrays/Easy/941-ValidMountainArray.ts
@@ -0,0 +1,20 @@
+/**
+    Easy
+    https://leetcode.com/problems/valid-mountain-array/
+*/
+function validMountainArray(arr: number[]): boolean {
+
+    if (arr.length < 3 || arr[0] > arr[1]) {
+        return false;
+    }
+
+    let decreasing = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            decreasing = true;
+        } else if (decreasing || arr[i] === arr[i + 1]) {
+            return false;
+        }
+    }
+    return decreasing;
+};
