21c6a27603ee37824721695617cce32146cffa76
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2024-01-25 14:16:35 +0200

    Solved problem number 487 Max Consecutive Ones II

---
+++ Arrays/Medium/487-MaxConsecutiveOnesII.ts
@@ -0,0 +1,18 @@
+/**
+    Medium
+    https://leetcode.com/problems/max-consecutive-ones-ii/
+*/
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let maxOnesCount = 0;
+    let onesCount = 0;
+    let countWithZero = 0;
+    for (const number of nums) {
+        onesCount++;
+        if (number === 0) {
+            countWithZero = onesCount;
+            onesCount = 0;
+        } 
+        maxOnesCount = Math.max(countWithZero + onesCount, maxOnesCount);
+    }
+    return maxOnesCount;
+};
