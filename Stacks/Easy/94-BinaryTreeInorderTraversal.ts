1e34fb2207a1cf9ba88b45a3a522624adb4d50a2
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2024-03-12 21:29:56 +0200

    Solved problem number 94 Binary Tree Inorder Traversal

---
+++ Stacks/Easy/94-BinaryTreeInorderTraversal.ts
@@ -0,0 +1,25 @@
+/**
+ * Easy
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ */
+function inorderTraversal(root: TreeNode | null): number[] {
+	const numbers = [];
+	// Create a stack to store the nodes to visit.
+	const toVisit = [];
+	// Start with the root of the tree.
+	let current = root;
+	while (toVisit.length || current) {
+		// Go to the leftmost node of the current subtree.
+		while (current) {
+			toVisit.push(current);
+			current = current.left;
+		}
+		// Get the leftmost node from the stack.
+		current = toVisit.pop();
+		// Add the value of the leftmost node to the result array.
+		numbers.push(current.val);
+		// Go to the right subtree.
+		current = current.right;
+	}
+	return numbers;
+}
