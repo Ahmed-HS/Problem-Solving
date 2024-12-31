083dd3dbc6206b12c9df42a397a26851e2f1dcc9
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2024-12-31 07:34:08 +0200

    Updated the solution to problem number 4 Median of Two Sorted Arrays

--- Arrays/Hard/4-MedianOfTwoSortedArrays.ts
+++ Arrays/Hard/4-MedianOfTwoSortedArrays.ts
@@ -26,32 +26,34 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 	let end = nums1.length;
 	while (start <= end) {
 		// The number of elements from nums1 and nums2 that should be included in the left half.
-		const num1Count = start + Math.trunc((end - start) / 2);
-		const num2Count = halfLength - num1Count;
-		// The maximum number from nums1 that should be included in the left half.
-		const num1Left = num1Count === 0 ? -Infinity : nums1[num1Count - 1];
-		// The minimum number from nums1 that should be included in the right half.
+		const nums1LeftCount = start + Math.trunc((end - start) / 2);
+		const nums2LeftCount = halfLength - nums1LeftCount;
+		// The last number from nums1 that should be included in the left half.
+		const num1Left =
+			nums1LeftCount === 0 ? -Infinity : nums1[nums1LeftCount - 1];
+		// The first number from nums1 that should be included in the right half.
 		const num1Right =
-			num1Count === nums1.length ? Infinity : nums1[num1Count];
-		// The maximum number from nums2 that should be included in the left half.
-		const num2Left = num2Count === 0 ? -Infinity : nums2[num2Count - 1];
-		// The minimum number from nums2 that should be included in the right half.
+			nums1LeftCount === nums1.length ? Infinity : nums1[nums1LeftCount];
+		// The last number from nums2 that should be included in the left half.
+		const num2Left =
+			nums2LeftCount === 0 ? -Infinity : nums2[nums2LeftCount - 1];
+		// The first number from nums2 that should be included in the right half.
 		const num2Right =
-			num2Count === nums2.length ? Infinity : nums2[num2Count];
+			nums2LeftCount === nums2.length ? Infinity : nums2[nums2LeftCount];
 		/**
-		 * If the maximum number from nums1 that should be included in the left half
-		 * is greater than the minimum number from nums2 that should be included in the right half,
+		 * If the last number from nums1 that should be included in the left half
+		 * is greater than the first number from nums2 that should be included in the right half,
 		 * Then the merge of the left half and the right half is not sorted.
 		 * We need to reduce the number of elements from nums1 that should be included in the left half.
-		 * If the maximum number from nums2 that should be included in the left half
-		 * is greater than the minimum number from nums1 that should be included in the right half,
+		 * If the last number from nums2 that should be included in the left half
+		 * is greater than the first number from nums1 that should be included in the right half,
 		 * Then the merge of the left half and the right half is not sorted.
 		 * We need to increase the number of elements from nums1 that should be included in the left half.
 		 */
 		if (num1Left > num2Right) {
-			end = num1Count - 1;
+			end = nums1LeftCount - 1;
 		} else if (num2Left > num1Right) {
-			start = num1Count + 1;
+			start = nums1LeftCount + 1;
 		} else {
 			/**
 			 * The merge of the left half and the right half is sorted.
