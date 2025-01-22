560a46602006952dcd3183add7de3a74fc150363
Ahmed Hatem <AhmedHatemSayed@gmail.com>
2025-01-22 08:06:31 +0200

    Solved problem number 12 Integer to Roman

---
+++ Strings/Medium/12-IntegerToRoman.ts
@@ -0,0 +1,30 @@
+/**
+ * Medium
+ * https://leetcode.com/problems/integer-to-roman/
+ */
+function intToRoman(number: number): string {
+	const romanToInt: [number, string][] = [
+		[1000, "M"],
+		[900, "CM"],
+		[500, "D"],
+		[400, "CD"],
+		[100, "C"],
+		[90, "XC"],
+		[50, "L"],
+		[40, "XL"],
+		[10, "X"],
+		[9, "IX"],
+		[5, "V"],
+		[4, "IV"],
+		[1, "I"],
+	];
+	let romanNumber = "";
+	for (const [int, roman] of romanToInt) {
+		while (number >= int) {
+			romanNumber += roman;
+			number -= int;
+		}
+		if (number === 0) break;
+	}
+	return romanNumber;
+}
